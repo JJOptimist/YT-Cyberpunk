7b36276df187d8a4202ce40925037b34d80bea12
JJOptimist <86833563+JJOptimist@users.noreply.github.com>
2022-12-20 16:30:27 +0100

    first commit

---
+++ Content.js
@@ -0,0 +1,19 @@
+//   --yt-spec-static-overlay-text-primary
+
+
+document.querySelector('ytd-app').style.setProperty('--yt-spec-base-background', '#091833');
+document.querySelector('yt-icon').style.setProperty('--yt-spec-brand-icon-inactive', '#00c08c'); 
+document.querySelector('#start').style.setProperty('--yt-spec-wordmark-text', '#0abdc6'); 
+document.querySelector('html, html[dark], [dark]').style.setProperty('--yt-spec-text-primary', '#0abdc6');
+document.querySelector('html, html[dark], [dark]').style.setProperty('--yt-spec-brand-icon-inactive', '#ea00d9');
+document.querySelector('#masthead').style.setProperty('--ytd-searchbox-legacy-border-color', '#ea00d9');
+document.querySelector('#masthead').style.setProperty('--ytd-searchbox-legacy-button-border-color', '#ea00d9');
+document.querySelector('#masthead').style.setProperty('--yt-spec-base-background', '#100114');
+document.querySelector('#masthead').style.setProperty('--yt-spec-icon-inactive', '#ea00d9');
+document.querySelector('#masthead').style.setProperty('--ytd-searchbox-background', '#100114');
+document.querySelector('#masthead').style.setProperty('--ytd-searchbox-text-color', '#ea00d9');
+document.querySelector('yt-touch-feedback-shape').style.setProperty('--yt-spec-static-overlay-text-primary', '#ea00d9');
+
+const text = document.querySelectorAll('#video-title');
+
+ 
